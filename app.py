ac8356c563d2c4fdb1b46081595267b24139196e
aryan-jadon <aryan.jadon@sjsu.edu>
2021-10-14 18:31:55 -0700

    refactored files

--- app.py
+++ app.py
@@ -1,23 +1,30 @@
-from collections import namedtuple
+import random
 from random import choice
-
 from flask import Flask, render_template
 
-Quote = namedtuple("Quote", ("text", "author"))
+images_path = [
+    "https://raw.githubusercontent.com/aryan-jadon/serverless-assignment-fall2021/main/templates/images/1.jpg",
+    "https://raw.githubusercontent.com/aryan-jadon/serverless-assignment-fall2021/main/templates/images/2.jpg",
+    "https://raw.githubusercontent.com/aryan-jadon/serverless-assignment-fall2021/main/templates/images/3.jpg",
+    "https://raw.githubusercontent.com/aryan-jadon/serverless-assignment-fall2021/main/templates/images/4.jpg",
+    "https://raw.githubusercontent.com/aryan-jadon/serverless-assignment-fall2021/main/templates/images/5.jpg",
+    "https://raw.githubusercontent.com/aryan-jadon/serverless-assignment-fall2021/main/templates/images/6.jpg"
+]
 
-quotes = [
-    Quote("Talk is cheap. Show me the code.", "Linus Torvalds"),
-    Quote("Programs must be written for people to read, and only incidentally for machines to execute.", "Harold Abelson"),
-    Quote("Always code as if the guy who ends up maintaining your code will be a violent psychopath who knows where you live",
-          "John Woods"),
-    Quote("Give a man a program, frustrate him for a day. Teach a man to program, frustrate him for a lifetime.", "Muhammad Waseem"),
-    Quote("Progress is possible only if we train ourselves to think about programs without thinking of them as pieces of executable code. ",
-          "Edsger W. Dijkstra")
+lazy_quotes = [
+    "In a cat's eye, all things belong to cats.",
+    "If cats could write history, their history would be mostly about cats.",
+    "Cats are connoisseurs of comfort.",
+    "As every cat owner knows, nobody owns a cat.",
+    "If cats could talk, they wouldn’t.",
+    "The only thing a cat worries about is what's happening right now."
 ]
 
 app = Flask(__name__)
 
 
-@app.route("/quote", methods=["GET"])
+@app.route("/", methods=["GET"])
 def get_random_quote():
-    return render_template('index.html')
+    return render_template('index.html',
+                           image_link=images_path[random.randint(0, 5)],
+                           quote=lazy_quotes[random.randint(0, 5)])
